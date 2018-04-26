1de4366ff810d9c25269da40046d3711ec8a8313
Zhenia Pasko <advokat.tm@gmail.com>
2018-04-26 14:57:37 +0300

    Initial Commit

---
+++ TradeOpteck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TradeOpteck
+//
+//  Created by Yevgenii Pasko on 4/26/18.
+//  Copyright © 2018 Yevgenii Pasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
